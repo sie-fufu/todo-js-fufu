7c666766f70b7647f1cd541d34a100b3122d19be
sie-fufu <69807259+sie-fufu@users.noreply.github.com>
2021-10-04 00:22:17 +0900

    js ファイル作成

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  const inputText = document.getElementById("add-text");
+  document.getElementById("add-text").value = "";
+
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  const li = document.createElement("li");
+  console.log(li);
+
+  const p = document.createElement("p");
+  p.className = "todo";
+  p.innerText = inputText;
+
+  const button = document.createElement("button");
+  button.innerText = "戻す";
+
+  div.appendChild(p);
+  div.appendChild(button);
+  li.appendChild(div);
+};
+
+document.getElementById("add-button").addEventListener("click", onClickAdd);
