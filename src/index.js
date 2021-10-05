c4c1da3f5b8399b00696b275fdd77f4508ea3fcf
sie-fufu <69807259+sie-fufu@users.noreply.github.com>
2021-10-05 10:57:30 +0900

    initial commit

--- src/index.js
+++ src/index.js
@@ -15,15 +15,27 @@ const onClickAdd = () => {
   const p = document.createElement("p");
   p.className = "todo";
   p.innerText = inputText;
-  console.log(p);
 
-  const button = document.createElement("button");
-  button.innerText = "完了";
+  //button（完了)タグの生成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    console.log("完了");
+  });
+
+  //button（削除)タグの生成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    //押された削除ボタンの親タグ(li)を未完了リストから削除
+    const deleteTarget = div.parentNode;
+    console.log(deleteTarget);
+  });
 
   li.appendChild(div);
   div.appendChild(p);
-  div.appendChild(button);
-
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
   //未完了リストに追加
   document.getElementById("incomplete-list").appendChild(li);
 };
